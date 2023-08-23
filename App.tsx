97eb07cbbd7b8ca4635ea3f3ecdd689378404e0b
Vyctoria Karina <vyctoriak@gmail.com>
2023-08-23 15:18:39 +0200

    fonts configuration

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-5xl font-bold text-zinc-50">
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="font-alt text-5xl font-bold text-gray-50">
         Time capsule :)
       </Text>
       <StatusBar style="light" translucent />
