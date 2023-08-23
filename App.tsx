6cfcaf495e43c81b5efe491b01e26015ed8bba2f
Vyctoria Karina <vyctoriak@gmail.com>
2023-08-23 18:13:04 +0200

    installed libraries for svg image, add the image files in assets and creted home page

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
-
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NlwLogo from './src/assets/nlw-spacetime-logo.svg'
+import { styled } from 'nativewind'
+import React from 'react'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -20,11 +27,41 @@ export default function App() {
   }
 
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="font-alt text-5xl font-bold text-gray-50">
-        Time capsule :)
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NlwLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Your time campsule
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Collect memorable moments from your journey and share (if you want)
+            with the world!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="upppercase font-alt text-sm text-black">
+            Register memory
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Built with ❤️ by Vyctoria K.
       </Text>
+
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
